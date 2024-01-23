4c3b7fc6fbb01638813adb9e601eb76a5079b295
ayushjay <ayushtime@gmail.com>
2024-01-23 19:30:41 +0530

    added models

--- audio/blog/models.py
+++ audio/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_id = models.EmailField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=800)
+    audio = models.FileField(upload_to="static/audio/")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date = models.DateTimeField(
+        auto_now=True,
+    )
+    tags = TaggableManager()
