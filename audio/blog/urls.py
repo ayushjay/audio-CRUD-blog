7686176d2fe3d06f64e0452f95e91ebee26bc185
ayushjay <ayushtime@gmail.com>
2024-01-25 20:59:51 +0530

    blog app works

--- audio/blog/urls.py
+++ audio/blog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("posts/", PostListView.as_view(), name="post-list"),
     path("create/", PostCreateView.as_view(), name="post-create"),
     path("posts/<int:pk>", PostDetailView.as_view(), name="post-detail"),
-    path("delete/<str:pk>/", PostDeleteView.as_view(), name="post-delete"),
+    path("delete/<int:pk>/", PostDeleteView.as_view(), name="post-delete"),
 ]
 
 
